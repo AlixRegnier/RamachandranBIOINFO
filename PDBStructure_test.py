ae6ea270f0d84aff560c0af218ff8338c3653928
YounsElmny <youness.elammany@gmail.com>
2023-09-06 15:05:37 +0200

    Test création de fichier

---
+++ PDBStructure_test.py
@@ -0,0 +1,13 @@
+from AminoAcid import AminoAcid
+from Atom import Atom
+from Point import Point
+import os
+from PDBStructure import StructurePDB
+try :
+    iS = StructurePDB.readPDB("1TEY.pdb")
+    #iS[1].compute_dihedrals()
+    iS[1].write_dihedrals("angles_1TEY.txt")
+    assert(os.path.exists("angles_1TEY.txt"))
+    print("File exists")
+except :
+    print("file doesn't exist")
