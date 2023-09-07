475c993b796cf169a146cd52224772640715d2a9
Yannlefrancois <yannlefrancoiscopy@gmail.com>
2023-09-07 12:42:12 +0200

    slight change

--- daa.py
+++ daa.py
@@ -3,12 +3,12 @@ from PDBStructure import *
 
 if __name__=="__main__":
 
-    error_message="python daa.py [-d|-k] [k|e] [minpoints] [-phipsi|-chi] filename.pdb\n" + \
-                    "-d : dbscan clustering / -k : kmeans clustering\n"+\
-                        "[k|e] : number above 0\n"+\
-                            "minpoint : number above 0 - this parameter exist only if you have selecter dbscan\n"+\
-                            "-phipsi : to analyse the phi and psi angles / -chi : to analyse side chain chi angle \n" + \
-                                "filename.pdb : has to be a pdb file"
+    error_message="""python daa.py <-d|-k> <k|e> [minpoints] <-phipsi|-chi> filename.pdb
+    -d\tdbscan clustering / -k : kmeans clustering
+    [k|e]\tnumber above 0
+    minpoint\tnumber above 0 - this parameter exist only if you have selecter dbscan
+    -phipsi\tto analyse the phi and psi angles / -chi : to analyse side chain chi angle
+    filename.pdb\thas to be a pdb file"""
 
     try :
         if sys.argv[1] != '-k' and sys.argv[1] != '-d':
@@ -31,21 +31,19 @@ if __name__=="__main__":
         exit()
     
     if sys.argv[1] == '-k':
-        model_list=StructurePDB.readPDB(sys.argv[4])
-    else:
         model_list=StructurePDB.readPDB(sys.argv[3])
+    else:
+        model_list=StructurePDB.readPDB(sys.argv[4])
 
-    coordonates_list=[[],[]]
+    point_list=[]
     for i in range(0,len(model_list)):
         a=model_list[i].compute_dihedrals()
-        coordonates_list[0].extend(a[0])
-        coordonates_list[1].extend(a[1])
+        point_list.extend(a.get_phi_psi())
         model_list[i].write_dihedrals(f'phi_phi_angles_model_{i+1}')
     
     if sys.argv[1]=="-k":
-        
+        pass
 
-    if sys.argv[1]=="-d":
-
-
+    else:
+        pass
 
